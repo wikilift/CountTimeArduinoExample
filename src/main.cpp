c52b4960bec7a30dd79d2a9d4565a2e1186ae98c
Dani <86446562+wikilift@users.noreply.github.com>
2023-01-28 22:07:44 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup()
 {
   Serial.begin(115200);
   pinMode(inputToCount, INPUT_PULLUP);
-  Serial.println("Hello");
+ 
   flag = true;
  
 }
@@ -20,19 +20,19 @@ void loop()
   {
     if (flag)
     {
-      Serial.println("Empiezo a contar");
+      Serial.println("Counter started");
       myZero = giveMeTheDifference();
       flag = false;
     }
   }
   else if (digitalRead(inputToCount))
-  { // entro aqui
+  { 
     
     if (!flag)
     {
       unsigned long result = giveMeTheTimeElapses(myZero);
-      Serial.println("Termino de contar ahora debo lanzar mi función");
-      Serial.println("Han transcurrido-> " + String(result)+ "miliSegundos");
+      Serial.println("Counter finished,must launch my function");
+      Serial.println("Elapsed-> " + String(result)+ "miliseconds");
     }
 
     flag = true;
